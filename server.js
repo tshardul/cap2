d976280bfed872620eea650216f194a47c946539
tshardul <shardult16@gmail.com>
2020-11-01 17:49:01 +0530

    fourth

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
 const connectDb = require("./src/connection");
 const User = require("./src/User.model");
 
 const PORT = 8080;
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
   const users = await User.find();
 
-  res.json(users);
+  // res.json(users);
+  res.render('index', { users })
+
 });
 
 app.get("/user-create", async (req, res) => {
   const user = new User({ username: "userTest" });
+
+  await user.save().then(() => console.log("User created"));
+
+  res.send("User created \n");
+});
+
+app.get("/user-create/:name", async (req, res) => {
+  const user = new User({ username: req.params.name });
 
   await user.save().then(() => console.log("User created"));
 
